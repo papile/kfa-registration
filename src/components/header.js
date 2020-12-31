abe07001cbabe8fb5ad00e1e57b4ebfdc1831f67
Michael Papile <papile@aps.org>
2020-12-31 14:34:01 -0500

    fix logo padding'

--- src/components/header.js
+++ src/components/header.js
@@ -7,8 +7,8 @@ const Header = ({ siteTitle }) => (
   <header>
     <div className="">
       <nav className="flex items-center justify-between flex-wrap bg-blue-900 pl-4 py-4">
-        <div className="flex items-center flex-grow text-white mr-6">
-          <Image />
+        <div className="flex items-center flex-grow text-white pr-6">
+          <span><Image /></span>
           <span className="pl-4 font-bold lg:text-4xl md:text-3xl sm:text-2xl xs:text-lg text-gray-300 tracking-tight">Kayak Fishing Association of New York Registration</span>
         </div>
       </nav>  
