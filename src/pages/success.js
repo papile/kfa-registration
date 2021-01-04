1fe4980a2299cebab31a6dfebbaf973bd0f74110
Michael Papile <papile@aps.org>
2021-01-04 00:55:57 -0500

    add success page

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,14 +1,11 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Signature from "../components/signature"
 
-const IndexPage = () => (
+const SuccessPage = () => (
   <Layout>
     <h1 className="mt-10 pt-5">Success!</h1>
     <p>Your payment was successfully processed!</p>
   </Layout>
 )
 
-export default IndexPage
+export default SuccessPage
